583582b9776847ba7f6127f02d927d4fd2faba1d
Ronaldo Paulo da Silva Filho <ronaldo.filho@assert.ifpb.edu.br>
2024-08-10 18:29:58 -0300

    add list bottons to menu navigation

--- src/assets/components/utils/UtilsMenuDrawer.tsx
+++ src/assets/components/utils/UtilsMenuDrawer.tsx
@@ -1,6 +1,11 @@
 // src/components/UtilsMenuDrawer.tsx
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Drawer, List, ListItem, ListItemIcon, ListItemText, IconButton, Divider } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
+import ContactMailIcon from '@mui/icons-material/ContactMail';
 
 interface UtilsMenuDrawerProps {
   isOpen: boolean;
@@ -9,14 +14,40 @@ interface UtilsMenuDrawerProps {
 
 const UtilsMenuDrawer: React.FC<UtilsMenuDrawerProps> = ({ isOpen, toggleDrawer }) => {
   return (
-    <div style={{ ...styles.drawer, transform: isOpen ? 'translateX(0)' : 'translateX(-100%)' }}>
-      <button onClick={toggleDrawer}>Close Drawer</button>
-      <ul>
-        <li><Link to="/" onClick={toggleDrawer}>Home</Link></li>
-        <li><Link to="/about" onClick={toggleDrawer}>About</Link></li>
-        <li><Link to="/contact" onClick={toggleDrawer}>Contact</Link></li>
-      </ul>
-    </div>
+    <Drawer
+    variant='temporary'
+    anchor='left'
+    open={isOpen}
+    onClose={toggleDrawer}
+    sx={{ ...styles.drawer, transform: isOpen ? 'translateX(0)' : 'translateX(-100%)' }}
+    >
+      <div>
+        <IconButton onClick={toggleDrawer}>
+          <MenuIcon />
+        </IconButton>
+        <Divider></Divider>
+        <List>
+          <ListItem button onClick={toggleDrawer} component={Link} to='/'>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText>Home</ListItemText>
+          </ListItem>
+          <ListItem button onClick={toggleDrawer} component={Link} to='/about'>
+            <ListItemIcon>
+              <InfoIcon />
+            </ListItemIcon>
+            <ListItemText>About</ListItemText>
+          </ListItem>
+          <ListItem button onClick={toggleDrawer} component={Link} to='/contact'>
+            <ListItemIcon>
+              <ContactMailIcon />
+            </ListItemIcon>
+            <ListItemText>Contact</ListItemText>
+          </ListItem>
+        </List>
+      </div>
+    </Drawer>
   );
 };
 
@@ -25,7 +56,7 @@ const styles = {
     position: 'fixed',
     top: 0,
     left: 0,
-    width: '250px',
+    width: '100px',
     height: '100%',
     backgroundColor: '#FFF',
     boxShadow: '2px 0 5px rgba(0, 0, 0, 0.1)',
